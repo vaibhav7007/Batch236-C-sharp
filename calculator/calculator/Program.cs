c19fde5315618dc41e53467df196e4faedb143c6
vaibhav <vaibhavmehta524@gmail.com>
2022-12-09 18:41:41 +0530

    [vaibhav] calculator program

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace AdditionOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calculator calculator = new Calculator();
+            Console.WriteLine("please enter 1st number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            calculator.Addition(first, second);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ calculator/calculator/calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionOfTwoNumbers
+{
+    class Calculator
+    {
+        public void Addition(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + secondNumber;
+
+            Console.WriteLine("sum:" + result);
+        }
+    }
+}
