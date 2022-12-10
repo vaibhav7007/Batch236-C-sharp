3e0063de549087be52757891b0b9f7ad9e5e555f
vaibhav <vaibhavmehta524@gmail.com>
2022-12-10 11:15:40 +0530

    [vaibhav] ReverseNumber and word program

---
+++ reversenumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the word ");
+            string input = Convert.ToString(Console.ReadLine());
+            ReverseWord.reverseword(input);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ reversenumber/ReverseWord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    class ReverseWord
+    {
+        public static void reverseword(String name)
+        {
+            string reverse = " ";
+            char[] chars = name.ToCharArray();
+            Console.WriteLine("Display the character");
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.WriteLine(chars[i] + " ");
+            }
+            for (int i = 0; i < chars.Length; i++)
+            {
+                reverse = chars[i] + reverse;
+            }
+            Console.WriteLine("Reverse word is " + reverse);
+        }
+    }
